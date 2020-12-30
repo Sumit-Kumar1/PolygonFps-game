971971283958cc1a28a88463531d87d82d940936
Sumit kumar <56474834+Sumit-Kumar7796@users.noreply.github.com>
2020-12-30 23:11:45 +0530

    Applied NavMesh settings and test succesfully

---
+++ Polygon-FPS/Assets/Scripts/NavMeshTest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+public class NavMeshTest : MonoBehaviour
+{
+    public Transform goal;
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
---
+++ Polygon-FPS/Assets/Scripts/testCamFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testCamFollow : MonoBehaviour
+{
+    private Camera cam;
+    private GameObject player;
+    private Vector3 offset = new Vector3(0,3,10);
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = (Camera)FindObjectOfType<Camera>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     cam.transform.position = player.transform.position+offset;   
+    }
+}
