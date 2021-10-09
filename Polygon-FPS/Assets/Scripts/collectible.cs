ab04265844823bb73cc7f17786c411e6fe9c2260
Sumit Kumar <kumarsumitjat298@gmail.com>
2021-10-09 17:27:08 +0530

    moved crosshair to prefab folder

--- Polygon-FPS/Assets/Scripts/collectible.cs
+++ Polygon-FPS/Assets/Scripts/collectible.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class collectible : MonoBehaviour
 {
     // Start is called before the first frame update
     private void OnCollisionEnter(Collision other)
-    { 
-        
+    {
+        Destroy(this.gameObject);
     }
 }
