88161c2141f58d335f04508bf4dd407b6e8d793b
Sumit Kumar <kumarsumitjat298@gmail.com>
2021-10-09 17:21:18 +0530

    Added Test collectible and collectible script, Removed skeleton asset

---
+++ Polygon-FPS/Assets/Scripts/collectible.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectible : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision other)
+    { 
+        
+    }
+}
