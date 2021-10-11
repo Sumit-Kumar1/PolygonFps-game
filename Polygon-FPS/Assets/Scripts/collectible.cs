bfd3697784d2b733e1b66c5a9033512617388e80
Sumit Kumar <kumarsumitjat298@gmail.com>
2021-10-11 11:39:59 +0530

    Imported Health and Armor Prefabs

--- Polygon-FPS/Assets/Scripts/collectible.cs
+++ Polygon-FPS/Assets/Scripts/collectible.cs
@@ -1,17 +1,24 @@
-using System.Net;
 using UnityEngine;
 
 public class collectible : MonoBehaviour
 {
     private Collider colliderObj;
-    private void Awake() {
+    private string prefabName = "";
+    //[SerializeField] Animator animator;
+    private void Awake()
+    {
         colliderObj = this.GetComponent<Collider>();
+    }
+    private void Start()
+    {
+        prefabName = this.tag;
     }
     private void OnCollisionEnter(Collision other)
     {
         //TODO: make it detect collision from player, when collids it increase the required asset value and destroy itself
-        if(other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player")
         {
+            Debug.Log("Name is " + prefabName);
             Destroy(this.gameObject);
         }
     }
